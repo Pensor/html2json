b6591c292f94acb369a2a39540d3a314da6f78b7
Patrick <patrick.feldtmann@gmail.com>
2021-04-26 13:19:25 +0200

    add images & slider

--- script.js
+++ script.js
@@ -1,39 +1,73 @@
 const fs = require("fs");
 const cheerio = require("cheerio");
 
+function capitalize(s) {
+  if (typeof s !== "string") return "";
+  return s.charAt(0).toUpperCase() + s.slice(1).toLowerCase();
+}
+
+function createImage(brand, section) {
+  return {
+    imageSrc: `/images/brands/${brand}/${capitalize(section)}_Placeholder.jpg`,
+    caption: "Placeholder caption",
+    captionPos: "right",
+  };
+}
+
+let brand = process.argv[2] || "[brand]";
+
 const html = fs.readFileSync("content.html", "utf8");
 
 const $ = cheerio.load(html, null, false);
 
 let array = $.root().children().toArray();
 
-let fileName;
+let fileName = brand;
 
-let result = [];
+let slider = [];
+let body = [{ type: "pageTitle", content: brand.toUpperCase() }];
 
 array.forEach(element => {
-  if (element.name == "h1") {
-    result.push({ type: "pageTitle", content: $(element).text() });
-    fileName = $(element).text().toLowerCase().split(" ")[0];
-  }
   if (element.name == "h3") {
-    if ($(element).text() === "PRODUCTION") {
-      result.push({ type: "invite" });
+    const text = $(element).text();
+    const image = createImage(brand, capitalize(text));
+    let images = [];
+
+    if (text.toUpperCase() == "PRODUCTION") {
+      body.push({ type: "invite" });
     } else {
-      result.push({ type: "line" });
+      body.push({ type: "line" });
     }
-    result.push({
+
+    body.push({
       type: "title",
-      content: $(element).text(),
-      anchor: $(element).text().toLowerCase(),
+      content: text,
+      anchor: text.toLowerCase(),
     });
+
+    if (text.toUpperCase() == "STYLE") {
+      images = Array(4).fill(image, 0, 4);
+      body.push({ type: "image", images });
+    } else if (text.toUpperCase() == "HISTORY") {
+      images.push(image);
+      body.push({ type: "image", images });
+    }
+
+    slider.push({ text: capitalize(text), link: `#${text.toLowerCase()}` });
   }
 
   if (element.name == "p") {
-    result.push({ type: "text", content: [$(element).html()] });
+    body.push({ type: "text", content: [$(element).html()] });
   }
 });
 
-fs.writeFileSync(`${fileName}.json`, JSON.stringify(result, null, 4));
+body.splice(2, 0, createImage(brand, "Background"));
 
-console.log(`${fileName}.json file created`)
+const data = {
+  slider,
+  body,
+};
+
+fs.writeFileSync(`${fileName}.json`, JSON.stringify(data, null, 4));
+
+console.log(`${fileName}.json file created`);
